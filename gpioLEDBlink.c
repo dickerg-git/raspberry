80baa891fd43dcc11037f1ce7b8abb11b9fc4660
Roger Dickerson <rogerd@rmii.com>
2017-12-18 14:20:04 -0700

    Change to program init to stay in Supervisor mode.

--- gpioLEDBlink.c
+++ gpioLEDBlink.c
@@ -76,19 +76,12 @@
 
 
 /*****************************************************************************
-**                INTERNAL MACRO DEFINITIONS
-*****************************************************************************/
-#define GPIO_BANK0_ADDRESS              (SOC_GPIO_0_REGS)
-#define GPIO_LED_PIN_NUMBER             (13)
-#define GPIO_BANK1_ADDRESS              (SOC_GPIO_1_REGS)
-#define GPIO_RESET_PIN_NUMBER           (9)
-
-/*****************************************************************************
 **                INTERNAL FUNCTION PROTOTYPES
 *****************************************************************************/
 void HW_Setup_GPIO_AM335x(void);
 void HW_GPIO_BlinkLED(int turnOff);
 unsigned int HW_GPIO_Reset_switch(void);
+unsigned int HW_GPIO_Debug_switch(void);
 void BootFromFlash(void);
 
 static void Delay(unsigned int count);
@@ -105,12 +98,16 @@ int main()
     unsigned int N_Reset = 1;               /* State of the N_Reset pin. */
     unsigned int blinks = 10000;
 
+    CacheEnable(CACHE_ALL);
+
     /* Set up the AM335x GPIO pins as connected to this prototype board. */
     /* Set up pins for LED and Reset switch.                             */
     /* ALSO, init the interrupt controller Aintc.                        */
     HW_Setup_GPIO_AM335x();
 
     /* Setup and start the DMTimer2 to free run, generate interrupt.     */
+    /* This function will enable clocks for the DMTimer2 instance */
+    DMTimer2ModuleClkConfig();
     DMTimerDisable(SOC_DMTIMER_2_REGS);
     DMTimer2SetUp();
 
@@ -132,7 +129,7 @@ int main()
     }
 
     /* Do some real work now... */
-    if( N_Reset == 0 )
+    if( HW_GPIO_Debug_switch() == 0 )
        BootFromFlash();
 
     HW_GPIO_BlinkLED(1);
@@ -146,82 +143,6 @@ int main()
 static void Delay(volatile unsigned int count)
 {
     while(count--);
-}
-
-
-/*
-** A function to blink the LED OFF, then ON.
-** If requested, exit with the LED off again.
-** Wired to the GPIO0-13 pin.
-*/
-void HW_GPIO_BlinkLED(int turnOff)
-{
-    unsigned int delay;
-
-    /* Driving a logic HIGH on the GPIO pin. LED OFF */
-    GPIOPinWrite(GPIO_BANK0_ADDRESS,
-                 GPIO_LED_PIN_NUMBER,
-                 GPIO_PIN_HIGH);
-
-    delay = 0x5FFFF;
-    while( delay--);
-
-    /* Driving a logic LOW on the GPIO pin. LED ON */
-    GPIOPinWrite(GPIO_BANK0_ADDRESS,
-                 GPIO_LED_PIN_NUMBER,
-                 GPIO_PIN_LOW);
-
-    delay = 0x2FFFF;
-    while( delay--);
-
-    if (turnOff) {
-        /* Exit the routine with the LED OFF. */
-        GPIOPinWrite(GPIO_BANK0_ADDRESS,
-                     GPIO_LED_PIN_NUMBER,
-                     GPIO_PIN_HIGH);
-    }
-}
-
-
-/*
-** A function to return the state of the PROTO Reset button.
-** Reads the pin wired to GPIO1-09, returns 0 or 1.
-*/
-unsigned int HW_GPIO_Reset_switch(void)
-{
-
-    return(
-	   GPIOPinRead(GPIO_BANK1_ADDRESS,
-                   GPIO_RESET_PIN_NUMBER)
-       >> GPIO_RESET_PIN_NUMBER
-    );
-
-}
-
-/* Attempt to boot the PROTO card from it's Flash */
-void BootFromFlash(void)
-{
-	unsigned long volatile * from_ptr = (unsigned long volatile *) 0x08000000;
-	unsigned long volatile * to_ptr   = (unsigned long volatile *) 0x80000000;
-
-	/* Read the Flash code header length and address. */
-    unsigned long code_length  = *((long*)from_ptr++);
-    unsigned long code_address = *((long*)from_ptr++);
-
-    /* Verify the code address in the image points to DDR space. */
-    if (code_address != 0x80000000) return;
-    if (code_length > 10000000) return;
-
-    while( code_length > 0 ) {
-    	*to_ptr = *from_ptr;
-        // if(*to_ptr != *from_ptr ) while(1); /* Data Miscompare, HALT! */
-
-        to_ptr++;
-		from_ptr++;
-
-		code_length -= 4;
-    }
-
 }
 
 
@@ -245,16 +166,120 @@ static void DMTimer2SetUp(void)
 }
 
 
+#define GPIO_BANK0_ADDRESS              (SOC_GPIO_0_REGS)
+#define GPIO_BANK1_ADDRESS              (SOC_GPIO_1_REGS)
+
+/*
+** A function to blink the LED OFF, then ON.
+** If requested, exit with the LED off again.
+** Wired to the GPIO0-13 pin.
+*/
+void HW_GPIO_BlinkLED(int turnOff)
+{
+#define GPIO_LED_PIN_NUMBER             (13)
+
+	unsigned int delay;
+
+	/* Driving a logic HIGH on the GPIO pin. LED OFF */
+	GPIOPinWrite(GPIO_BANK0_ADDRESS,
+                 GPIO_LED_PIN_NUMBER,
+                 GPIO_PIN_HIGH);
+
+	delay = 0x5FFFF;
+	while( delay--);
+
+	/* Driving a logic LOW on the GPIO pin. LED ON */
+	GPIOPinWrite(GPIO_BANK0_ADDRESS,
+                 GPIO_LED_PIN_NUMBER,
+                 GPIO_PIN_LOW);
+
+    delay = 0x2FFFF;
+    while( delay--);
+
+    if (turnOff) {
+        /* Exit the routine with the LED OFF. */
+        GPIOPinWrite(GPIO_BANK0_ADDRESS,
+                     GPIO_LED_PIN_NUMBER,
+                     GPIO_PIN_HIGH);
+    }
+}
+
+
+/*
+** A function to return the state of the PROTO Reset button.
+** Reads the pin wired to GPIO1-09, returns 0 or 1.
+*/
+unsigned int HW_GPIO_Reset_switch(void)
+{
+#define GPIO_RESET_PIN_NUMBER           (9)
+
+    return(
+	   GPIOPinRead(GPIO_BANK1_ADDRESS,
+                   GPIO_RESET_PIN_NUMBER)
+       >> GPIO_RESET_PIN_NUMBER
+    );
+
+}
+
+
+/*
+** A function to return the state of the PROTO Debug button.
+** Reads the pin wired to GPIO1-08, returns 0 or 1.
+*/
+unsigned int HW_GPIO_Debug_switch(void)
+{
+#define GPIO_RESET_PIN_NUMBER           (8)
+
+    return(
+	   GPIOPinRead(GPIO_BANK1_ADDRESS,
+                   GPIO_RESET_PIN_NUMBER)
+       >> GPIO_RESET_PIN_NUMBER
+    );
+
+}
+
+
+/* Attempt to boot the PROTO card from it's Flash */
+void BootFromFlash(void)
+{
+	unsigned long volatile * from_ptr = (unsigned long volatile *) 0x08000000;
+	unsigned long volatile * to_ptr   = (unsigned long volatile *) 0x80000000;
+
+	/* Read the Flash code header length and address. */
+    unsigned long code_length  = *((long*)from_ptr++);
+    unsigned long code_address = *((long*)from_ptr++);
+    unsigned long code_bytes = 0;
+
+    /* Turn off the D-Cache, to get Write-Thru into the DDR. */
+    CacheDataCleanAll();
+    CacheDisable(CACHE_DCACHE);
+
+    /* Verify the code address in the image points to DDR space. */
+    if (code_address != 0x80000000) return;
+    if (code_length > 10000000) return;
+
+    while( code_length > 0 ) {
+    	*to_ptr = *from_ptr;
+        if(*to_ptr != *from_ptr ) while(1); /* Data Miscompare, HALT! */
+
+        to_ptr++;
+		from_ptr++;
+
+		code_length -= 4;
+        code_bytes  += 4;
+    }
+
+}
+
+
 void HW_Setup_GPIO_AM335x(void)
 {
     /* Enabling functional clocks for the GPIO instances.         */
     GPIO0ModuleClkConfig();
     GPIO1ModuleClkConfig();
 
-    /* This function will enable clocks for the DMTimer2 instance */
-    DMTimer2ModuleClkConfig();
-
     /* Selecting GPIO0[13] GPIO1[09] pin for use.       */
+    /* These routines need Supervisor mode privileges.  */
     GPIO0Pin13PinMuxSetup();  /* GPIO0-13 OUTPUT to LED */
     GPIO1Pin09PinMuxSetup();  /* GPIO1-09 INPUT/PULL-UP */
 
